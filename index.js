4b7e316749c1afabc4984043e62e95e9512d1043
umerfarooq6699 <umer66997@gmail.com>
2024-10-28 00:04:54 -0700

    new changes added

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const dashboardRouter=require("./Routers/dashboardRouter")
 
 connection()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"https://hangree-shopping-store.vercel.app"
+}))
 
 app.use("/",userRouter)
 app.use("/",dashboardRouter)
